ef2ec3d8596f9df6cbd7caeac787827d7eba626d
RukshanTK <43653261+RukshanTK@users.noreply.github.com>
2019-06-21 12:25:02 +0530

    Add files via upload

---
+++ AF-Project/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/courseWeb');
+mongoose.Promise = global.Promise;
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.use(function (err, req, res, next) {
+   // console.log(err);
+    res.status(422).send({error: err.message})
+});
+
+//app.use('/courseWeb', routes);
+
+app.listen(process.env.port || 4000, function () {
+
+    console.log("Now listening for requests");
+});
